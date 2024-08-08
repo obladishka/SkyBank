1add3d86bd475df194bdb19fc7ea79ae269738c1
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-08 07:41:24 +0300

    update get_data_from_xlsx func according to tests

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -8,3 +9,21 @@ def get_data_from_xlsx(file_path: str) -> pd.DataFrame | None:
         return df
     except FileNotFoundError:
         print("Файл не найден. Проверьте правильность введенных данных.")
+        data = {
+            "Дата операции": [np.nan],
+            "Дата платежа": [np.nan],
+            "Номер карты": [np.nan],
+            "Статус": [np.nan],
+            "Сумма операции": [np.nan],
+            "Валюта операции": [np.nan],
+            "Сумма платежа": [np.nan],
+            "Валюта платежа": [np.nan],
+            "Кэшбэк": [np.nan],
+            "Категория": [np.nan],
+            "MCC": [np.nan],
+            "Описание": [np.nan],
+            "Бонусы (включая кэшбэк)": [np.nan],
+            "Округление на инвесткопилку": [np.nan],
+            "Сумма операции с округлением": [np.nan],
+        }
+        return pd.DataFrame(data)
