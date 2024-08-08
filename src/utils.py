6e3eb4e4de0acacce23dcb8b1d483e1c731e31db
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-08 21:42:29 +0300

    add func for getting user settings

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import numpy as np
 import pandas as pd
 
@@ -27,3 +30,80 @@ def get_data_from_xlsx(file_path: str) -> pd.DataFrame | None:
             "Сумма операции с округлением": [np.nan],
         }
         return pd.DataFrame(data)
+
+
+def get_data_from_user() -> None:
+    """Функция для задания пользовательских настроек."""
+
+    file_path = os.path.dirname(os.path.dirname(__file__))
+
+    # Получаем список кодов существующих валют
+    try:
+        with open(os.path.join(file_path, "data", "currencies.json"), "r", encoding="utf-8") as cf:
+            try:
+                currencies = json.load(cf)
+                codes = [currency.get("code") for currency in currencies]
+            except json.JSONDecodeError:
+                codes = []
+    except FileNotFoundError:
+        print("Файл не найден. Проверьте правильность введенных данных.")
+        codes = []
+
+    # Получаем список тикеров компаний S&P 500
+    try:
+        with open(os.path.join(file_path, "data", "sandp500.json"), "r", encoding="utf-8") as sf:
+            try:
+                companies = json.load(sf)
+                symbols = [company.get("tickerSymbol") for company in companies]
+            except json.JSONDecodeError:
+                symbols = []
+    except FileNotFoundError:
+        print("Файл не найден. Проверьте правильность введенных данных.")
+        symbols = []
+
+    # Получаем данные от пользователя и проверяем их корректность
+    user_currencies = (
+        input("Введите ISO-коды валют, для отображения на главной странице (через запятую или пробел): ")
+        .upper()
+        .replace(",", " ")
+        .replace("  ", " ")
+        .split()
+    )
+
+    while any(currency not in codes for currency in user_currencies):
+        print("Введенные валюты не найдены. Проверьте правильность введенных данных.")
+        user_currencies = (
+            input("Введите ISO-коды валют, для отображения на главной странице (через запятую или пробел): ")
+            .upper()
+            .replace(",", " ")
+            .replace("  ", " ")
+            .split()
+        )
+
+    user_stocks = (
+        input(
+            "Введите тикеры акций компаний S&P 500, для отображения на главной странице (через запятую или пробел): "
+        )
+        .upper()
+        .replace(",", " ")
+        .replace("  ", " ")
+        .split()
+    )
+
+    while any(stock not in symbols for stock in user_stocks):
+        print("Введенные тикеры не найдены. Проверьте правильность введенных данных.")
+        user_stocks = (
+            input(
+                "Введите тикеры акций компаний S&P 500, для отображения на главной странице\
+                 (через запятую или пробел): "
+            )
+            .upper()
+            .replace(",", " ")
+            .replace("  ", " ")
+            .split()
+        )
+
+    # Вносим пользовательские данные в файл
+    with open(os.path.join(file_path, "user_settings.json"), "w", encoding="utf-8") as of:
+        user_settings = {"user_currencies": user_currencies, "user_stocks": user_stocks}
+        json.dump(user_settings, of)
