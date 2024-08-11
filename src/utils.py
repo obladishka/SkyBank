5164c262e862a0fa2e83f35c733f96ad505704b8
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-11 14:01:52 +0300

    reorganize modules

--- src/utils.py
+++ src/utils.py
@@ -117,6 +117,36 @@ def calculate_cashback(operations_dict: dict) -> dict:
     return operations_dict
 
 
+def say_hello(time: int) -> str:
+    """Функция для вывода приветствия в зависимости от текущего времени."""
+    if 0 <= time <= 23:
+        return (
+            "Доброй ночи"
+            if 0 <= time < 5
+            else "Доброе утро" if 5 <= time < 12 else "Добрый день" if 12 <= time < 18 else "Добрый вечер"
+        )
+    return "Неверно указано время. Проверьте правильность введенных данных."
+
+
+def process_cards_info(date: str, file_path: str) -> list[dict]:
+    """Функция для вывода информации по каждой карте."""
+
+    all_operations = get_data_from_xlsx(file_path)
+    current_month_operations = filter_by_date(date, all_operations)
+    current_month_expenses = get_total_expenses(current_month_operations)
+    operations_dict = calculate_cashback(current_month_expenses)
+
+    cards = [key for key in operations_dict.keys() if key != "nan"]
+    return [
+        {
+            "last_digits": card,
+            "total_spent": round(operations_dict.get(card)[0], 2),
+            "cashback": operations_dict.get(card)[1],
+        }
+        for card in cards
+    ]
+
+
 def get_currencies(currencies_file: str) -> list:
     """Функция для получения списка существующих валют."""
     try:
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ import requests
 
 from src.utils import (calculate_cashback, filter_by_date, get_currencies, get_data_from_user, get_data_from_xlsx,
                        get_data_via_api_currencies, get_data_via_api_stocks, get_stocks, get_total_expenses,
-                       sort_by_amount)
+                       sort_by_amount, process_cards_info, say_hello)
 
 
 @patch("src.utils.pd.read_excel")
@@ -89,6 +89,88 @@ def test_calculate_cashback(monthly_operations, cashback):
 def test_calculate_cashback_zero_expenses():
     """Тестирует работу функции, когда отсутствуют данные по операциям."""
     assert calculate_cashback({"nan": 0.0}) == {"nan": [0.0, 0.0]}
+
+
+@pytest.mark.parametrize(
+    "time, expected",
+    [
+        (0, "Доброй ночи"),
+        (4, "Доброй ночи"),
+        (5, "Доброе утро"),
+        (11, "Доброе утро"),
+        (12, "Добрый день"),
+        (15, "Добрый день"),
+        (18, "Добрый вечер"),
+        (23, "Добрый вечер"),
+    ],
+)
+def test_say_hello(time, expected):
+    """Тестирует нормальную работу функции с различными входными данными."""
+    assert say_hello(time) == expected
+
+
+def test_say_hello_wrong_time():
+    """Тестирует работу функции, когда передано неверное время."""
+    assert say_hello(24) == "Неверно указано время. Проверьте правильность введенных данных."
+
+
+@patch("src.utils.calculate_cashback")
+@patch("src.utils.get_total_expenses")
+@patch("src.utils.filter_by_date")
+@patch("src.utils.get_data_from_xlsx")
+def test_process_cards_info(
+    mock_get_df, mock_filter_df, mock_total_expenses, mock_cashback, get_df, monthly_operations, cashback
+):
+    """Тестирует нормальную работу функции."""
+    mock_get_df.return_value = get_df
+    mock_filter_df.return_value = get_df
+    mock_total_expenses.return_value = monthly_operations
+    mock_cashback.return_value = cashback
+    assert process_cards_info("30.11.2021 18:19:28", "existing.xlsx")[:3] == [
+        {
+            "last_digits": "1112",
+            "total_spent": 46207.08,
+            "cashback": 462.07,
+        },
+        {
+            "last_digits": "4556",
+            "total_spent": 533948.75,
+            "cashback": 5339.49,
+        },
+        {
+            "last_digits": "5091",
+            "total_spent": 14918.16,
+            "cashback": 149.18,
+        },
+    ]
+    mock_get_df.assert_called_once_with("existing.xlsx")
+    mock_filter_df.assert_called_once_with("30.11.2021 18:19:28", get_df)
+
+
+@pytest.mark.parametrize(
+    "date, file",
+    [
+        ("30.11.2021 18:19:28", "no_such_file.xlsx"),
+        ("30.11.2021", "existing.xlsx"),
+        ("08-03-2022 15:45:00", "existing.xlsx"),
+        ("08-03-2022 15:45:00", "no_such_file.xlsx"),
+    ],
+)
+@patch("src.utils.calculate_cashback")
+@patch("src.utils.get_total_expenses")
+@patch("src.utils.filter_by_date")
+@patch("src.utils.get_data_from_xlsx")
+def test_process_cards_info_empty_data(
+    mock_get_df, mock_filter_df, mock_total_expenses, mock_cashback, get_empty_df, date, file
+):
+    """Тестирует работу функции, когда переданы неверные входные данные."""
+    mock_get_df.return_value = get_empty_df
+    mock_filter_df.return_value = get_empty_df
+    mock_total_expenses.return_value = {"nan": 0.0}
+    mock_cashback.return_value = {"nan": [0.0, 0.0]}
+    assert process_cards_info(date, file) == []
+    mock_get_df.assert_called_once_with(file)
+    mock_filter_df.assert_called_once_with(date, get_empty_df)
 
 
 @patch("src.utils.json.load")
