36c2d319b0d67c4e978717820d3f24b60a0b9cbc
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-13 21:46:14 +0300

    add decorator

--- src/reports.py
+++ src/reports.py
@@ -1,8 +1,10 @@
+import io
 import json
 import logging
 import os
 from datetime import datetime, timedelta
-from typing import Optional
+from functools import wraps
+from typing import Any, Optional
 
 import pandas as pd
 
@@ -15,6 +17,36 @@ file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(me
 
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+
+
+def write_to_file(my_file: str = None) -> Any:
+    """Декоратор для записи в файл результата, возвращаемого функцией для формирования отчета."""
+
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
+
+    def my_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            result = func(*args, **kwargs)
+
+            if not my_file:
+                file_name = os.path.join(file_path, "report.json")
+                with open(file_name, "w", encoding="utf-8") as output_file:
+                    new_result = json.loads(result)
+                    json.dump(new_result, output_file, ensure_ascii=False, indent=4)
+
+            elif my_file[-4:] == ".csv":
+                file_name = os.path.join(file_path, my_file)
+                pd.read_json(io.StringIO(result)).to_csv(file_name, index=False)
+
+            else:
+                file_name = os.path.join(file_path, my_file)
+                pd.read_json(io.StringIO(result)).to_excel(file_name, index=False)
+
+        return wrapper
+
+    return my_decorator
 
 
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> str:
